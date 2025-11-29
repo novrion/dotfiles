150fca703762a740ca853eadcfbe342759cd5ff9
novrion <elindstenz@outlook.com>
2025-11-29 21:52:51 +0100

    added nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,34 @@
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.swapfile = false
+vim.opt.clipboard = "unnamedplus"
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+})
+
+require "mini.pick".setup()
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
+vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+
+vim.lsp.enable({ "lua_ls", "clangd", "pylsp", "bashls" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>da', '<cmd>lua vim.diagnostic.setqflist()<CR>', { noremap = true, silent = true })
+
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
